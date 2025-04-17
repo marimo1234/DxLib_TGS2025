5d2a8d3b6fc5f8aa165f68bfe6fe710a5ad9e6c4
hiziki444 <ryota-uechi-k24f@stu.kbc.ac.jp>
2025-04-17 10:52:49 +0900

    海の画像を挿入、表示

--- DxLib_TGS2025/Src/Object/map.cpp
+++ DxLib_TGS2025/Src/Object/map.cpp
@@ -2,10 +2,11 @@
 #include"map.h"
 
 int ground;
-
+int sea;
 void MapInit(void)
 {
 	ground = LoadGraph("Resource/images/GroundReef.png");
+	sea = LoadGraph("Resource/images/art_119.png");
 }
 void MapUpdate(void)
 {
@@ -13,6 +14,6 @@ void MapUpdate(void)
 }
 void MapDraw(int stage)
 {
-	
+	DrawRotaGraphF(640, 380, 1.0, 0.0, sea, TRUE);
 	DrawRotaGraphF(640,380,0.5+stage,0.0, ground, TRUE);
 }
