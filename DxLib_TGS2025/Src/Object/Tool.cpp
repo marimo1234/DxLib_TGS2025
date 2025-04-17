e70fe085ef562a034ac8e151f9139cd6f44e2bf2
yara12345-0 <yuta-yara-k24h@stu.kbc.ac.jp>
2025-04-17 11:55:46 +0900

    アイテム画像描画

--- DxLib_TGS2025/Src/Object/Tool.cpp
+++ DxLib_TGS2025/Src/Object/Tool.cpp
@@ -7,20 +7,29 @@
 #define FRAME_Y			(700)
 #define STONETILE_X		(955)
 #define STONETILE_Y		(655)
+#define LOGTILE_X		(1035)
+#define LOGTILE_Y		(675)
+#define AX_X			(1160)
+#define AX_Y			(670)
 
-int pickaxe;
-int item_frame;
-int stonetile;
+int pickaxe_img;
+int itemframe_img;
+int stonetile_img;
+int logtile_img;
+int ax_img;
 
 void ToolInit(void)
 {
 	//アイテム枠画像読み込み
-	item_frame = LoadGraph("Resource/images/item_frame.png");
+	itemframe_img = LoadGraph("Resource/images/item_frame.png");
 	//ピッケル画像読み込み
-	pickaxe=LoadGraph("Resource/images/pickaxe.png");
+	pickaxe_img=LoadGraph("Resource/images/pickaxe.png");
 	//石の地面画像の読み込み
-	stonetile=LoadGraph("Resource/images/stone_tiles.png");
-
+	stonetile_img=LoadGraph("Resource/images/stone_tiles.png");
+	//丸太の地面画像の読み込み
+	logtile_img = LoadGraph("Resource/images/Log.png");
+	//斧の画像読み込み
+	ax_img = LoadGraph("Resource/images/ax.png");
 }
 void ToolManagerUpdate(void)
 {
@@ -29,9 +38,13 @@ void ToolManagerUpdate(void)
 void ToolDraw(void) 
 {
 	//アイテム枠の描画
-	DrawRotaGraph(FRAME_X, FRAME_Y, 1.0, 0.0, item_frame, TRUE);
+	DrawRotaGraph(FRAME_X, FRAME_Y, 1.0, 0.0, itemframe_img, TRUE);
 	//ピッケルの描画（アイテム枠）
-	DrawRotaGraph(PICKAXE_X, PICKAXE_Y,0.15,0.0,pickaxe, TRUE);
+	DrawRotaGraph(PICKAXE_X, PICKAXE_Y,0.15,0.0,pickaxe_img, TRUE);
 	//石の地面の描画（アイテム枠）
-	DrawRotaGraph(STONETILE_X, STONETILE_Y,0.3,0.0,stonetile, TRUE);
+	DrawRotaGraph(STONETILE_X, STONETILE_Y,0.3,0.0,stonetile_img, TRUE);
+	//丸太の地面の描画（アイテム枠）
+	DrawRotaGraph(LOGTILE_X, LOGTILE_Y, 0.4, 0.0, logtile_img , TRUE);
+	//斧の描画（アイテム枠）
+	DrawRotaGraph(AX_X, AX_Y, 0.15, 0.0, ax_img, TRUE);
 }
