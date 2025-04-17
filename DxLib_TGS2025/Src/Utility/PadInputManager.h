00ff22fd6d414b5a117423caae932f084506099a
sakura2026 <oura-yano-k24f@stu.kbc.ac.jp>
2025-04-17 10:16:42 +0900

    カーソル画像（仮）を追加
    仮のカーソルです

--- DxLib_TGS2025/Src/Utility/PadInputManager.h
+++ DxLib_TGS2025/Src/Utility/PadInputManager.h
@@ -1,1 +1,12 @@
 #pragma once
+
+#define PAD_BUTTON_MAX (16)            // ゲームパッドのボタンの最大数
+#define PAD_STICK_MAX  (32767.0f)      // スティックを傾けたときの最大数
+
+enum class ePadInputState : unsigned char
+{
+	eNone,        // 未入力
+	ePress,       // ボタンを押した瞬間
+	eRelease,     // ボタンを押した瞬間
+	eHold,        // ボタンを押し続けている
+};
