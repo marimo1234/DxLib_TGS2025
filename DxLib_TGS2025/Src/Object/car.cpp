37d9781aaaff49407f5e5b0f1964f1b465efc378
marimo1234 <souma-matsuda-k24f@stu.kbc.ac.jp>
2025-04-18 11:56:40 +0900

    車の描画

--- DxLib_TGS2025/Src/Object/car.cpp
+++ DxLib_TGS2025/Src/Object/car.cpp
@@ -5,7 +5,8 @@ int car_image[2] = {};
 
 void CarInit(void)
 {
-	/*car_image[0]=LoadDivGraph()*/
+	car_image[0] = LoadGraph("Resource/images/left_car.png");
+	car_image[1] = LoadGraph("Resource/images/front_car.png");
 }
 
 void CarManagerUpdate(void)
@@ -15,5 +16,6 @@ void CarManagerUpdate(void)
 
 void CarDraw(void)
 {
-
+	DrawRotaGraph(300, 300, 1.0, 0.0, car_image[0], TRUE);
+	DrawRotaGraph(380, 300, 1.0, 0.0, car_image[1], TRUE);
 }
