73edf6cc014a7c5a25d5a2197ce72a92ad6142ec
marimo1234 <souma-matsuda-k24f@stu.kbc.ac.jp>
2025-04-19 23:42:39 +0900

    木の描画とアニメーションの土台

--- DxLib_TGS2025/Src/Object/WoodRock.cpp
+++ DxLib_TGS2025/Src/Object/WoodRock.cpp
@@ -1,20 +1,80 @@
 #include "DxLib.h"
 #include "WoodRock.h"
+#include "../Utility/InputManager.h"
 
-int tree_image;
-int rock_image;
+int wood_image[4] = {};
+int rock_image[4] = {};
+int wood_animation;
+int wood_hitcount;
 
+//初期化
 void WoodRockInit(void)
 {
+	wood_hitcount = eHit0;
+	wood_animation = wood_image[0];
 
+	wood_image[0]= LoadGraph("Resource/images/Wood0.png");
+	wood_image[1]= LoadGraph("Resource/images/Wood1.png");
+	wood_image[2]= LoadGraph("Resource/images/Wood2.png");
+	wood_image[3]= LoadGraph("Resource/images/Wood3.png");
 }
 
+//更新
 void WoodRockUpdate(void)
 {
-
+	//木のアニメーション
+	WoodAnimation();
 }
 
+//描画処理
 void WoodRockDraw(void)
 {
 
+	DrawRotaGraph(280, 200, 1.0, 0.0, wood_animation, TRUE);
+	DrawRotaGraph(200, 200, 1.0, 0.0, wood_image[0], TRUE);
+	DrawRotaGraph(200, 280, 1.0, 0.0, wood_image[1], TRUE);
+	DrawRotaGraph(200, 360, 1.0, 0.0, wood_image[2], TRUE);
+	DrawRotaGraph(200, 440, 1.0, 0.0, wood_image[3], TRUE);
+}
+
+//Aキーを押したら木の描画する画像を変える
+//後々Hitした回数で木の描画する画像を変えるようにする
+void WoodAnimation(void)
+{
+		switch (wood_hitcount)
+		{
+		case eHit0:
+			wood_animation = wood_image[0];
+			if (GetKeyInputState(KEY_INPUT_A) == ePress)
+			{
+				wood_hitcount = eHit1;
+			}
+			break;
+
+		case eHit1:
+			wood_animation = wood_image[1];
+			if (GetKeyInputState(KEY_INPUT_A) == ePress)
+			{
+				wood_hitcount = eHit2;
+			}
+			
+			break;
+
+		case eHit2:
+			wood_animation = wood_image[2];
+			if (GetKeyInputState(KEY_INPUT_A) == ePress)
+			{
+				wood_hitcount = eHit3;
+			}
+			break;
+
+		case eHit3:
+			wood_animation = wood_image[3];
+			if (GetKeyInputState(KEY_INPUT_A) == ePress)
+			{
+				wood_hitcount = eHit0;     //今だけループするようにしている
+			}
+			break;
+		}
+	
 }
--- DxLib_TGS2025/Src/Object/WoodRock.h
+++ DxLib_TGS2025/Src/Object/WoodRock.h
@@ -1,4 +1,15 @@
 #pragma once
+
+enum eWoodRockState
+{
+	eHit0,
+	eHit1,
+	eHit2,
+	eHit3,
+};
+
 void WoodRockInit(void);
 void WoodRockUpdate(void);
 void WoodRockDraw(void);
+
+void WoodAnimation(void);
