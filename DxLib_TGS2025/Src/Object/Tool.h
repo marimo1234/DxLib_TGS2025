3f2ca200fb24252af1883608c08ced000e0285f0
yara12345-0 <yuta-yara-k24h@stu.kbc.ac.jp>
2025-05-06 23:17:36 +0900

    ツール描画変更

--- DxLib_TGS2025/Src/Object/Tool.h
+++ DxLib_TGS2025/Src/Object/Tool.h
@@ -21,8 +21,10 @@ struct Tool
 	int item_number;	//選択しているアイテムの種類
 	int road_img;		//道の画像ハンドル
 	int road_num;		//道の所持数
+	int road_flag;		//道を置くときのフラグ
 	int wood_road_img;	//丸太の道の画像ハンドル
 	int wood_road_num;	//丸太の道の所持数
+	int wood_road_flag;
 	int rock_sub_flag;	//岩の数を引くフラグ
 	int wood_sub_flag;	//木の数を引くフラグ
 	float draw_x[10];	//描画座標ｘ
@@ -34,9 +36,11 @@ void ToolManagerUpdate(void);
 void ToolDraw(void);
 void Move_Frame(void);
 void Put_Road(void);
-void Draw_Road(void);
+void Put_Wood_Road(void);
 void Sub_Num(void);
 void Tool_Reset(void);
+void Road_Flag_off(void);
+void Wood_Road_Flag_off(void);
 
 
 const Tool* Get_Tool(void);
