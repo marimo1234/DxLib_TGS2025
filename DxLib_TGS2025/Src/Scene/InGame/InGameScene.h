7c99b147fc64190ca26b61dd4b15e522a851b26a
marimo1234 <souma-matsuda-k24f@stu.kbc.ac.jp>
2025-05-26 16:57:17 +0900

    あ

--- DxLib_TGS2025/Src/Scene/InGame/InGameScene.h
+++ DxLib_TGS2025/Src/Scene/InGame/InGameScene.h
@@ -22,6 +22,12 @@ struct InGame
 	int manual_image;
 	int space;
 	int back;
+	bool menu_flag;
+	int menu_num;
+	int menu_image;
+	int menu_cursor;
+	float menu_cursor_x;
+    float menu_cursor_y;
 };
 
 
@@ -33,4 +39,5 @@ const InGame* GetInGame(void);
 
 void GameStart();
 void StageChange(void);
+void InGameMenu(void);
 //void StageChange(const Goal* goal);
