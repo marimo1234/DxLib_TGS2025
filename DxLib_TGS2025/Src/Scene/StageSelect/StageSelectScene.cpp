4f8199663d2d0a927fbc11d9341e6b772bf82db7
marimo1234 <souma-matsuda-k24f@stu.kbc.ac.jp>
2025-05-27 16:08:32 +0900

    画像挿入

--- DxLib_TGS2025/Src/Scene/StageSelect/StageSelectScene.cpp
+++ DxLib_TGS2025/Src/Scene/StageSelect/StageSelectScene.cpp
@@ -13,10 +13,29 @@ StageSelect stageselect;
 //リザルト画面初期化
 void StageSelectSceneInit(void)
 {
-	stageselect.background_image = 0;
-	stageselect.position.x=0.0f;
-	stageselect.position.y=0.0f;
+	stageselect.position.x = 0.0f;
+	stageselect.position.y = 0.0f;
 	stageselect.number = 0;
+
+	for (int j = 0; j < 2; j++)
+	{
+		for (int i = 0; i < 3; i++)
+		{
+			stageselect.torut_array[i][j] = -1;
+		}
+	}
+
+
+	stageselect.background_image = LoadGraph("Resource/images/StageSelect.png");
+	stageselect.trout_image[0] = LoadGraph("Resource/images/StageTrout.png");
+	/*stageselect.trout_image[1] = LoadGraph("Resource/images/StageSelect.png");*/
+
+
+    stageselect.number_image[0] = LoadGraph("Resource/images/1.png");
+    stageselect.number_image[1] = LoadGraph("Resource/images/2.png");
+    stageselect.number_image[2] = LoadGraph("Resource/images/3.png");
+    stageselect.number_image[3] = LoadGraph("Resource/images/4.png");
+    stageselect.number_image[4] = LoadGraph("Resource/images/5.png");
 }
 
 //リザルトシーンの更新
@@ -35,7 +54,13 @@ eSceneType StageSelectSceneUpdate(void)
 //リザルトシーンの描画
 void StageSelectSceneDraw(void)
 {
-	/*DrawRotaGraph(stageselect.position.x, stageselect.position.y, 0.0, 1.0, stageselect.background_image, TRUE);*/
+	DrawRotaGraphF(640.0f, 360.0f, 1.0, 0.0, stageselect.background_image, TRUE);
+	for (int i = 0; i < 6; i++)
+	{
+		DrawRotaGraphF(400.0f, 360.0f, 0.3, 0.0, stageselect.trout_image[0], TRUE);
+	}
+	
+	DrawRotaGraphF(640.0f, 360.0f, 0.3, 0.0, stageselect.number_image[0], TRUE);
 	DrawExtendFormatString(470, 360, 2.0, 2.0, GetColor(255, 255, 255), "Xボタンでインゲーム画面へ");
 	/*DrawFormatString(100, 100, GetColor(255, 255, 255), "zでタイトル画面へ");*/
 }
--- DxLib_TGS2025/Src/Scene/StageSelect/StageSelectScene.h
+++ DxLib_TGS2025/Src/Scene/StageSelect/StageSelectScene.h
@@ -7,6 +7,9 @@
 struct StageSelect
 {
 	int background_image;
+	int trout_image[2];
+	int number_image[5];
+	int torut_array[3][2];
 	Vector2D position;
 	int number;
 };
