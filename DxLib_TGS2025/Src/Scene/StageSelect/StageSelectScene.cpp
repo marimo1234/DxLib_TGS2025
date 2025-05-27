ef79f6422cad0c59a306b250e610cc921c83d9bc
marimo1234 <souma-matsuda-k24f@stu.kbc.ac.jp>
2025-05-27 17:24:50 +0900

    セレクト画面制作中

--- DxLib_TGS2025/Src/Scene/StageSelect/StageSelectScene.cpp
+++ DxLib_TGS2025/Src/Scene/StageSelect/StageSelectScene.cpp
@@ -13,10 +13,12 @@ StageSelect stageselect;
 //リザルト画面初期化
 void StageSelectSceneInit(void)
 {
-	stageselect.position.x = 0.0f;
-	stageselect.position.y = 0.0f;
+	stageselect.position.x = 400.0f;
+	stageselect.position.y = 435.0f;
 	stageselect.number = 0;
 	stageselect.array_number = 0;
+	stageselect.array_x = 0;
+	stageselect.array_y = 0;
 
 
 	for (int j = 0; j < 2; j++)
@@ -30,7 +32,7 @@ void StageSelectSceneInit(void)
 
 	stageselect.background_image = LoadGraph("Resource/images/StageSelect.png");
 	stageselect.trout_image[0] = LoadGraph("Resource/images/StageTrout.png");
-	/*stageselect.trout_image[1] = LoadGraph("Resource/images/StageSelect.png");*/
+	stageselect.trout_image[1] = LoadGraph("Resource/images/StageTrout2.png");
 
 
     stageselect.number_image[0] = LoadGraph("Resource/images/1.png");
@@ -38,6 +40,9 @@ void StageSelectSceneInit(void)
     stageselect.number_image[2] = LoadGraph("Resource/images/3.png");
     stageselect.number_image[3] = LoadGraph("Resource/images/4.png");
     stageselect.number_image[4] = LoadGraph("Resource/images/5.png");
+
+	stageselect.car_image= LoadGraph("Resource/images/car2_right.png");
+	
 }
 
 //リザルトシーンの更新
@@ -57,7 +62,6 @@ eSceneType StageSelectSceneUpdate(void)
 void StageSelectSceneDraw(void)
 {
 	DrawRotaGraphF(640.0f, 360.0f, 1.0, 0.0, stageselect.background_image, TRUE);
-
 	stageselect.array_number = 0;
 	for (int j = 0; j < 2; j++)
 	{
@@ -71,9 +75,10 @@ void StageSelectSceneDraw(void)
 				DrawRotaGraphF(400.0f + i * 240.0f, 235.0f + j * 200.0f, 0.4, 0.0, stageselect.number_image[stageselect.array_number], TRUE);
 				stageselect.array_number++;
 			}
-			
 		}
 	}
+
+	DrawRotaGraph(400.0f, 435.0f, 0.2, 0.0, stageselect.car_image, TRUE);
 	
 	DrawExtendFormatString(470, 360, 2.0, 2.0, GetColor(255, 255, 255), "Xボタンでインゲーム画面へ");
 	/*DrawFormatString(100, 100, GetColor(255, 255, 255), "zでタイトル画面へ");*/
@@ -85,3 +90,79 @@ const StageSelect* GetStageSelect(void)
 }
 
 
+void CursolButtonMovement(void)
+{
+
+	PadInputManager* pad_input = PadInputManager::GetInstance();
+
+	if (pad_input->GetButtonInputState(XINPUT_BUTTON_DPAD_LEFT) == ePadInputState::ePress)
+	{
+
+		// 十字ボタンの左を押したとき
+		if (stageselect.array_x > 0)
+		{
+			stageselect.array_x--;
+			stageselect.position.x = 240.0f * stageselect.array_x + 200.0f;
+
+		}
+		else if (stageselect.array_x == 0)
+		{
+			stageselect.array_x = 2;
+			stageselect.position.x = 240.0f * stageselect.array_x + 200.0f;
+		}
+
+		// 移動のSE（もし使うならここに入れてね）
+
+	}
+	else if (pad_input->GetButtonInputState(XINPUT_BUTTON_DPAD_RIGHT) == ePadInputState::ePress)
+	{
+		// 十字ボタンの右を押したとき
+		if (stageselect.array_x < 2)
+		{
+			stageselect.array_x++;
+			stageselect.position.x = 240.0f * stageselect.array_x + 200.0f;
+
+		}
+		else if (stageselect.array_x == 2)
+		{
+			stageselect.array_x = 0;
+			stageselect.position.x = 240.0f * stageselect.array_x + 200.0f;
+		}
+		// 移動のSE（左とおんなじ音入れてね）
+
+	}
+	else if (pad_input->GetButtonInputState(XINPUT_BUTTON_DPAD_UP) == ePadInputState::ePress)
+	{
+		// 十字ボタンの上を押したとき
+		if (stageselect.array_y > 0)
+		{
+			stageselect.array_y--;
+			stageselect.position.y = 200.0f * stageselect.array_y + 120.0f;
+
+		}
+		else if (stageselect.array_y == 0)
+		{
+			stageselect.array_y = 1;
+			stageselect.position.y = 200.0f * stageselect.array_y + 120.0f;
+		}
+		// 移動のSE（左とおんなじ音入れてね）
+
+	}
+	else if (pad_input->GetButtonInputState(XINPUT_BUTTON_DPAD_DOWN) == ePadInputState::ePress)
+	{
+		// 十字ボタンの下を押したとき
+		if (stageselect.array_y < 1)
+		{
+			stageselect.array_y++;
+			stageselect.position.y = 200.0f * stageselect.array_y + 120.0f;
+
+		}
+		else if (stageselect.array_y == 1)
+		{
+			stageselect.array_y = 0;
+			stageselect.position.y = 200.0f * stageselect.array_y + 120.0f;
+		}
+		// 移動のSE（左とおんなじ音入れてね）
+
+	}
+}
--- DxLib_TGS2025/Src/Scene/StageSelect/StageSelectScene.h
+++ DxLib_TGS2025/Src/Scene/StageSelect/StageSelectScene.h
@@ -7,12 +7,15 @@
 struct StageSelect
 {
 	int background_image;
+	int car_image;
 	int trout_image[2];
 	int number_image[5];
 	int torut_array[3][2];
 	Vector2D position;
 	int number;
 	int array_number;
+	int array_x;
+	int array_y;
 };
 
 void StageSelectSceneInit(void);
