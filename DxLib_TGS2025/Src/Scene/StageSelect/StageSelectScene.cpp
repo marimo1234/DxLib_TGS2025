ae98b11b9437e67e141e7971b3d26ccc522bcced
marimo1234 <souma-matsuda-k24f@stu.kbc.ac.jp>
2025-05-27 15:18:19 +0900

    変数制作

--- DxLib_TGS2025/Src/Scene/StageSelect/StageSelectScene.cpp
+++ DxLib_TGS2025/Src/Scene/StageSelect/StageSelectScene.cpp
@@ -1,15 +1,22 @@
 ﻿#include "StageSelectScene.h"
 #include "../../Utility/InputManager.h"
 #include "../../Utility/PadInputManager.h"
+#include"../InGame/InGameScene.h"
+
 #include "DxLib.h"
 #include <stdlib.h>
 
 int result_score;		//表示するスコアの値
 
+
+StageSelect stageselect;
 //リザルト画面初期化
 void StageSelectSceneInit(void)
 {
-
+	stageselect.background_image = 0;
+	stageselect.position.x=0.0f;
+	stageselect.position.y=0.0f;
+	stageselect.number = 0;
 }
 
 //リザルトシーンの更新
@@ -28,7 +35,14 @@ eSceneType StageSelectSceneUpdate(void)
 //リザルトシーンの描画
 void StageSelectSceneDraw(void)
 {
+	/*DrawRotaGraph(stageselect.position.x, stageselect.position.y, 0.0, 1.0, stageselect.background_image, TRUE);*/
 	DrawExtendFormatString(470, 360, 2.0, 2.0, GetColor(255, 255, 255), "Xボタンでインゲーム画面へ");
 	/*DrawFormatString(100, 100, GetColor(255, 255, 255), "zでタイトル画面へ");*/
 }
+
+const StageSelect* GetStageSelect(void)
+{
+	return &stageselect;
+}
+
 
--- DxLib_TGS2025/Src/Scene/StageSelect/StageSelectScene.h
+++ DxLib_TGS2025/Src/Scene/StageSelect/StageSelectScene.h
@@ -1,7 +1,18 @@
 ﻿#pragma once
 
 #include "../SceneType.h"
+#include "../../Utility/Geometry.h"
+
+
+struct StageSelect
+{
+	int background_image;
+	Vector2D position;
+	int number;
+};
 
 void StageSelectSceneInit(void);
 eSceneType StageSelectSceneUpdate(void);
 void StageSelectSceneDraw(void);
+
+const StageSelect*GetStageSelect(void);
