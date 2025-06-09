22c2ad72c8ab9bdd0f63fd50379a9bb1cf75d8dd
marimo1234 <souma-matsuda-k24f@stu.kbc.ac.jp>
2025-06-09 10:34:26 +0900

    Titleの基盤を作った

--- DxLib_TGS2025/Src/Scene/Title/TitleScene.h
+++ DxLib_TGS2025/Src/Scene/Title/TitleScene.h
@@ -5,9 +5,16 @@
 struct Title
 {
 	int image;
+	int char_image[3];
+	int cursor_image;
+	int char_num;
+	float cursor_x;
+	float cursor_y;
 };
 void TitleSceneInit(void);
 eSceneType TitleSceneUpdate(void);
 void TitleSceneDraw(void);
+
+void TitleCursorUpdate(void);
 
 const Title* GetTitle(void);
