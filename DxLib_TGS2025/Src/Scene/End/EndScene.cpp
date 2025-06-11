25c1f4078069b1cbebd2a5d3431b71980b4e5281
hiziki444 <ryota-uechi-k24f@stu.kbc.ac.jp>
2025-06-11 16:30:06 +0900

    adadadadaa

--- DxLib_TGS2025/Src/Scene/End/EndScene.cpp
+++ DxLib_TGS2025/Src/Scene/End/EndScene.cpp
@@ -3,6 +3,11 @@
 #include "DxLib.h"
 
 End end;
+int black_count1 = end.fps / 3;
+int black_count2 = 0;
+int black_count3 = 0;
+int fps2=0;
+int fps3=0;
 //ヘルプシーン初期化
 void EndSceneInit(void)
 {
@@ -26,7 +31,7 @@ eSceneType EndSceneUpdate(void)
 		return eTitle;								//タイトル画面へ
 	}
 
-	if (end.fps > 300)
+	if (end.fps > 600)
 	{
 		return eEnd2;
 	}
@@ -40,19 +45,35 @@ eSceneType EndSceneUpdate(void)
 void EndSceneDraw(void)
 {
 	
-	end.black_count = end.fps / 20;
+	black_count1 = end.fps / 3;
 	DrawRotaGraphF(640.0f, 360.0f, 1.0, 0.0, end.image, TRUE);
-	if (end.black_count >= 6)
+	if (end.fps >= 1)
 	{
-		DrawRotaGraph(850 + 110 * end.black_count, 200, 1.0, 0.0, end.black, TRUE);
-		DrawRotaGraph(850 + 110 * end.black_count, 350, 1.0, 0.0, end.black, TRUE);
-		DrawRotaGraph(850 + 110 * end.black_count, 500, 1.0, 0.0, end.black, TRUE);
+		DrawRotaGraph(840 + 110 * black_count1, 200, 1.0, 0.0, end.black, TRUE);
 	}
 	else
 	{
-		DrawRotaGraph(750 + 110 * end.black_count, 200, 1.0, 0.0, end.black, TRUE);
-		DrawRotaGraph(750 + 110 * end.black_count, 350, 1.0, 0.0, end.black, TRUE);
-		DrawRotaGraph(750 + 110 * end.black_count, 500, 1.0, 0.0, end.black, TRUE);
+		DrawRotaGraph(750 + 110 * black_count1, 200, 1.0, 0.0, end.black, TRUE);
+	}
+	if (end.fps >= 35)
+	{
+		fps2++;
+		black_count2 = fps2 / 3;
+		DrawRotaGraph(850 + 110 * black_count2, 350, 0.75, 0.0, end.black, TRUE);
+	}
+	else
+	{
+		DrawRotaGraph(750 + 110 * black_count2, 350, 0.75, 0.0, end.black, TRUE);
+	}
+	if (end.fps >= 50)
+	{
+		fps3++;
+		black_count3 += fps3 / 3;
+		DrawRotaGraph(650 + 110 * black_count3, 550, 1.0, 0.0, end.black, TRUE);
+	}
+	else
+	{
+		DrawRotaGraph(850 + 110 * black_count3, 550, 1.0, 0.0, end.black, TRUE);
 	}
 	DrawFormatString(100, 100, GetColor(255, 255, 255), "%d", end.black_count);
 	//SetFontSize(40);
